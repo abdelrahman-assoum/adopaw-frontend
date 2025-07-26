40feaa48f27734987c3b541e646a18eb8e213480
mimic00-cpu <naimb2798@gmail.com>
2025-07-26 23:39:24 +0300

    colors edit

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -3,6 +3,10 @@ const colors = {
   blue: '#339DFF',     // Blue 500
   coral: '#FF4F4F',    // Coral 500
   textLight: '#FFFFFF',
+  textDark: '#000000',
+  inputBg: '#F5F5F5',
+  inputBorder: '#CCCCCC',
+  placeholder: '#999999',
 };
 
 export default colors;
